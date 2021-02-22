a784f520d39a5e5295a577fc0742ebc8adb86df5
Ahmed Raza Kashmire Butt <ark.butt@gmail.com>
2021-02-22 22:31:23 +0100

    Mad Lib Game of a specific story

---
+++ MadLibsGame/MadLibsGame.py
@@ -0,0 +1,10 @@
+# def MadLib(sequence):
+print("After hiding the painting in his "+'"n_o_u_n"'+ " for two years, he grew " + '"a_d_j_a_c_t_i_v_e"' + " and tried to sell it to a/an "+'"n_o_u_n"'+" in florence, but was caught." )
+print("Write a noun:")
+noun=raw_input()
+print("Write an adjactive:")
+adjactive= raw_input()
+print("Write an noun:")
+noun2= raw_input()
+print("After hiding the painting in his "+noun+ " for two years, he grew " + adjactive + " and tried to sell it to a/an "+noun2+" in florence, but was caught." )
+
